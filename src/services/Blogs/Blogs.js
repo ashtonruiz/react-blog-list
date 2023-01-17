5594509671d193c9dcaad910925e052c4b6ec6a9
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-17 14:55:18 -0800

    had an extra console log

--- src/services/Blogs/Blogs.js
+++ src/services/Blogs/Blogs.js
@@ -2,6 +2,5 @@ import { checkError, client } from '../client.js';
 
 export async function getBlogs() {
   const response = await client.from('blogs').select('*');
-  console.log(response, 'this one');
   return checkError(response);
 }
