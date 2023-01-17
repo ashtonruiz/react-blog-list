642f2d3d920d8ecdd9685ba7df13d1c64c959c5b
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-17 12:36:46 -0800

    started pseudo coding the useEffect hook and realized I will probably need a blogs.js file so I created that.

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import getBlogs from '../../Blogs/Blogs.js';
+
+// const [blogs, setBlogs] = useState([]);
+// useEffect(() => {
+//   const fetchData = async () => {
+//     const resp = await getBlogs();
+//     setBlogs(resp);
+//   };
+//   fetchData();
+// });
